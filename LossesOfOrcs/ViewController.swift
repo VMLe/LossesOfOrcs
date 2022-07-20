6adf50a85b177e2c94881b8ca2b72199346b408f
Vitalii Levchenko <vmlevch@gmail.com>
2022-07-20 16:53:31 +0300

    Initial Commit

---
+++ LossesOfOrcs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LossesOfOrcs
+//
+//  Created by Vitalii Levchenko on 20.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
